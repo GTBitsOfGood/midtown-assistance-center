76f84e11b4548d1ddebd6e71d39c26d169fb6993
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-09-25 00:14:50 -0400

    Added react-bootstrap modal

--- src/homePage.js
+++ src/homePage.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import {Button, Modal} from 'react-bootstrap';
 
 class StudentSignUpForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      value: null,
+      showModal: false,
     };
+  }
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+    this.setState({ showModal: true });
   }
 
   render() {
     return (
       <div>
-        <h1> Hello {this.props.name} </h1>
-        <button className="square">
-          Test button
-        </button>
+        <p>Click to get the full Modal experience!</p>
+
+        <Button bsStyle="success" bsSize="large" onClick={this.open}>
+          Launch demo modal
+        </Button>
+
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            TODO modal body
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
@@ -30,4 +52,5 @@ StudentSignUpForm.defaultProps = {
   name: 'Stranger'
 };
 
-ReactDOM.render(<StudentSignUpForm name='Student'/>, document.getElementById('root'));
+ReactDOM.render(<StudentSignUpForm name='Student'/>,
+  document.getElementById('root'));
