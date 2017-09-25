06690f4415e4c40e2aac7e6b3a9c5cd280bbacc3
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-09-24 23:25:27 -0400

    Basic react component with prop validation

--- src/homePage.js
+++ src/homePage.js
@@ -1,4 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(<h1>hellooooooo</h1>, document.getElementById('root'));
+class StudentSignUpForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Hello {this.props.name} </h1>
+        <button className="square">
+          Test button
+        </button>
+      </div>
+    );
+  }
+}
+
+StudentSignUpForm.propTypes = {
+  name: PropTypes.string
+};
+
+ReactDOM.render(<StudentSignUpForm name="student"/>, document.getElementById('root'));
