ab85f4337a57c25e811448ed84d93f3f74dcc470
Sam Baek <kie827@gmail.com>
2017-09-25 19:34:50 -0400

    Implement home menu bar using react-bootstrap

--- src/homePage.js
+++ src/homePage.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+
+const divStyle = {
+  background: '#EEB211',
+  text: 'black'
+};
 
 export class MenuBarComponent extends React.Component {
   constructor(props) {
@@ -9,7 +15,23 @@ export class MenuBarComponent extends React.Component {
 
   render() {
     return (
-
+      <Navbar collapseOnSelect style={divStyle}>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Midtown Assistance Center</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullLeft>
+            <NavItem>About us</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Sign In</NavItem>
+            <NavItem eventKey={2} href="#">Sign Up</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
