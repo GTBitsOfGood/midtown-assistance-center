5388dafc1e5388d9b1aa7a15a82058a9ccaeeeeb
Sam Baek <kie827@gmail.com>
2017-09-26 19:41:53 -0400

    Add Student model

--- models/Student.js
+++ models/Student.js
@@ -2,7 +2,20 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-
+  first_name: {type: String, required: true},
+  last_name: {type: String, required: true},
+  email: {type: String, required: true},
+  _id: {type: String, required: true},
+  password: {type: String, required: true},
+  join_date: {type: Date, default: Date.now, required: true},
+  bio: {type: String},
+  profile_picture: {type: String, default: ''},
+  classroom: {type: String, required: true},
+  grade_level: {type: Number, required: true}
 });
 
-module.exports = mongoose.model('', schema);
+schema.virtual('username').get(function() {
+  return this._id;
+});
+
+module.exports = mongoose.model('Student', schema);
