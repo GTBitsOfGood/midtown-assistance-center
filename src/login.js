bd9a61396c57fd40c1dc80ce77c3e053f4cdb92f
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-09-25 21:40:57 -0400

    tried to fix css and image

--- src/login.js
+++ src/login.js
@@ -2,26 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './Homepage.css';
 import './login.css'
-import './mac.jpg'
+// import './mac.jpg'
 
 class Loginpage extends React.Component {
     constructor(props) {
         super(props);
     }
-
-    // render() {
-    //     return (
-    //       <div className="wrap">
-    //         <div><h1>Hello Everyone</h1></div>
-    //       </div>
-    //     );
-    // }
-
     render() {
         return (
           <div className="wrap">
             <div className="header">
-                <img src="mac.jpg" alt="" height="100%">
+                <img src="mac.jpg" alt="" height="100%"></img>
                 <button type="button">About</button>
                 <button type="button">Sign Up</button>
                 <button type="button">Login</button>
@@ -30,9 +21,9 @@ class Loginpage extends React.Component {
                 <div className="formarea">
                     <h1>Login</h1>
                     <form action="/action.php">
-                      <input type="text" name="fname" placeholder="Username"><br>
-                      <input type="Password" name="lname" placeholder="Password"><br>
-                      <input type="submit" value="Submit">
+                      <input type="text" name="fname" placeholder="Username"></input><br></br>
+                      <input type="Password" name="lname" placeholder="Password"></input><br></br>
+                      <input type="submit" value="Submit"></input>
                     </form> 
                 </div>
             </div>
@@ -42,6 +33,7 @@ class Loginpage extends React.Component {
     }
 }
 
+export default Loginpage;
 
 // export default Homepage;
 // ReactDOM.render(<h1>hello</h1>, document.getElementById('root'));
