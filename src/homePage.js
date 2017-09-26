2aaad04db245116ea69da122a6b9eb0cc65453b2
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-09-25 21:13:09 -0400

    I did the login page

--- src/homePage.js
+++ src/homePage.js
@@ -1,4 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Homepage.css';
 
-ReactDOM.render(<h1>hello</h1>, document.getElementById('root'));
+class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+          <div className="wrap">
+            <div><h1>Hello Everyone</h1></div>
+          </div>
+        );
+    }
+}
+
+
+// export default Homepage;
+// ReactDOM.render(<h1>hello</h1>, document.getElementById('root'));
+ReactDOM.render(<Homepage/>, document.getElementById('root'));
