6b0d0b31aa40f28cad2a056e0ba62309f9b982dc
Sam Baek <kie827@gmail.com>
2017-09-26 20:10:04 -0400

    Implement checkIfUsernameIsTaken()

--- api/dao.js
+++ api/dao.js
@@ -1,19 +1,40 @@
-function checkIfUsernameIsTaken(username) {
+import Tutor from '../models/Tutor';
+import Student from '../models/Student';
 
-}
+module.exports = {
+  checkIfUsernameIsTaken: function(username, callback) {
+    Tutor.find({_id: username}, function (err, docs) {
+      if (err) {
+        console.log('Error checking if username is taken: ', err);
+      } else if (docs.length > 0) {
+        callback(true);
+      } else {
+        Student.find({_id: username}, function (err, docs) {
+          if (err) {
+            console.log('Error checking if username is taken: ', err);
+          } else if (docs.length > 0) {
+            callback(true);
+          } else {
+            callback(false);
+          }
+        });
+      }
+    });
+  },
 
-function checkIfEmailIsTaken(email) {
+  checkIfEmailIsTaken: function(email) {
 
-}
+  },
 
-function validateAccessCode(accessFolder) {
+  validateAccessCode: function(accessFolder) {
 
-}
+  },
 
-function createUser(user) {
+  createUser: function(user) {
 
-}
+  },
 
-function getUser(loginDetails) {
+  getUser: function(loginDetails) {
 
-}
+  }
+};
