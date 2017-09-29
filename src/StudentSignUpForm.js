2b75cab122ec002a1c290f0910c1f2975c522f38
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-09-29 16:34:05 -0400

    eslint fixes

--- src/StudentSignUpForm.js
+++ src/StudentSignUpForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Button, Modal, ControlLabel, Form, HelpBlock} from 'react-bootstrap';
-
+import {Form, HelpBlock} from 'react-bootstrap';
 
 class StudentSignUpForm extends React.Component {
   constructor(props) {
