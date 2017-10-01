c988f526efd9018758a6f80fc2eb52f340663af1
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-09-30 21:06:03 -0400

    Standardize logging

--- api/dao/access_code_dao.js
+++ api/dao/access_code_dao.js
@@ -3,13 +3,16 @@ const AccessCode = require('../../models/AccessCode');
 module.exports = {
 
   validateAccessCode: function (accessFolder, callback) {
+
     AccessCode.find({code: accessFolder}, function (err, docs) {
       if (err) {
-        console.log('Error checking retrieving school is taken: ', err);
+        console.error('Error checking retrieving school is taken: ', err);
+
       } else if (docs.length === 1) {
         callback(docs[0]);
       } else if (docs.length > 1) {
-        console.log('Multiple access code:', accessFolder);
+        console.warn('Multiple access code:', accessFolder);
+
       } else {
         callback(null);
       }
