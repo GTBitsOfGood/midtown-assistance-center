593e76cfd88031aa796c3eabe6473bc50013ae58
Sam Baek <kie827@gmail.com>
2017-10-20 18:34:14 -0400

    bland home page

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 import HomeMenuBar from './HomeMenuBar.jsx';
-import SignUpTabs from './SignUpTabs.jsx'
+import SignUpTabs from './SignUpTabs.jsx';
 import LoginPage from './form/LoginForm.jsx';
+import AboutUs from './AboutUs.jsx';
 import styles from '../../public/css/login_signup.css';
+
 
 const HomeComp = function() {
   return (
     <div>
       <HomeMenuBar/>
       <Router history={browserHistory}>
+        <Route path="/home" component={AboutUs}/>
         <Route path="/home/login" component={LoginPage}/>
         <Route path="/home/signUp" component={SignUpTabs}/>
       </Router>
