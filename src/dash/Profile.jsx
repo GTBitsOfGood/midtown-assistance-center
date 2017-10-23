f96e30f53a105c77d980d315b10559a372d69292
Sam Baek <kie827@gmail.com>
2017-10-23 14:50:37 -0400

    Add hardcoded fake info

--- src/dash/Profile.jsx
+++ src/dash/Profile.jsx
@@ -3,6 +3,19 @@ import { connect } from 'react-redux';
 import { setUserAction } from '../redux/userActions.js';
 import axios from 'axios';
 
+const student = {
+    first_name: 'Sammy',
+    last_name: 'Baek',
+    email: 'asdf@email.com',
+    // _id: {type: String, required: true},
+    password: 'asdfqwer',
+    join_date: Date.now(),
+    bio: 'Currently this is hardcoded json. The username and password are from redux',
+    // profile_picture: ,
+    classroom: 'asdf1234',
+    grade_level: 9
+};
+
 class Profile extends React.Component {
     constructor(props) {
         super(props);
@@ -22,6 +35,8 @@ class Profile extends React.Component {
             });
     }
 
+
+
     render() {
         {this.props.user}
         return (
@@ -29,20 +44,27 @@ class Profile extends React.Component {
             <div className="container">
                 <br/>
                 <div className="row">
-                    <div className="col-xs-12 col-sm-6 col-md-6">
+                    <div className="col-xs-12">
                         <div className="well well-sm">
                             <div className="row">
                                 <div className="col-sm-6 col-md-4">
                                     <img src="../../images/default_user_img.png" alt="" className="img-rounded img-responsive" />
                                 </div>
                                 <div className="col-sm-6 col-md-8">
-                                    <h4>{ this.props.user }</h4>
+                                    <h2>{ this.props.user }</h2>
                                     <small><cite title="Atlanta, USA">Atlanta, USA <i className="glyphicon glyphicon-map-marker">
                                     </i></cite></small>
-                                    <p>
-                                        <i className="glyphicon glyphicon-envelope"></i> asdf@email.com
+                                    <h4>
+                                        <i className="glyphicon glyphicon-envelope"></i> { student.email }
                                         <br />
-                                        <i className="glyphicon glyphicon-gift"></i> June 02, 1988</p>
+                                        <i className="glyphicon glyphicon-lock"></i> { this.props.password }
+                                        <br/>
+                                        <i className="glyphicon glyphicon-calendar"></i> { student.join_date }
+                                        <br/>
+                                        <i className="glyphicon glyphicon-apple"></i> { student.classroom }
+                                        <br/>
+                                        <i className="glyphicon glyphicon-home"></i> { student.bio }
+                                    </h4>
                                     <div className="btn-group">
                                         <button type="button" className="btn btn-primary">
                                             Social</button>
