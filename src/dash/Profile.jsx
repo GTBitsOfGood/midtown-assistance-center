6aef5ca02a1a675925c01d6e5b7b3dcc72394abe
Sam Baek <kie827@gmail.com>
2017-10-23 14:50:52 -0400

    Remove btn group at bottom

--- src/dash/Profile.jsx
+++ src/dash/Profile.jsx
@@ -65,20 +65,6 @@ class Profile extends React.Component {
                                         <br/>
                                         <i className="glyphicon glyphicon-home"></i> { student.bio }
                                     </h4>
-                                    <div className="btn-group">
-                                        <button type="button" className="btn btn-primary">
-                                            Social</button>
-                                        <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                                            <span className="caret"></span><span className="sr-only">Social</span>
-                                        </button>
-                                        <ul className="dropdown-menu" role="menu">
-                                            <li><a href="#">Twitter</a></li>
-                                            <li><a href="https://plus.google.com/+Jquery2dotnet/posts">Google +</a></li>
-                                            <li><a href="https://www.facebook.com/jquery2dotnet">Facebook</a></li>
-                                            <li className="divider"></li>
-                                            <li><a href="#">Github</a></li>
-                                        </ul>
-                                    </div>
                                 </div>
                             </div>
                         </div>
