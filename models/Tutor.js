5258d664cac4a3e3e860909b7e538c3864aaa950
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-10-28 23:01:01 -0400

    Fixed tutor schema

--- models/Tutor.js
+++ models/Tutor.js
@@ -7,74 +7,100 @@ let schema = new Schema({
     email: {type: String, required: true},
     _id: {type: String, required: true},
     password: {type: String, required: true},
-    join_date: {type: Date, default: Date.now, required: false},
-    status: {type: String, default: 'in review', required: false},
+    join_date: {type: Date, default: Date.now, required: true},
+    status: {type: String, default: 'in review', required: true},
     profile_picture: {type: String, default: ''},
     bio: {type: String},
     resume: {type: String, default: ''},
     gpa: {type: Number},
     transcript: {type: String, default: ''},
     rating: {type: Number, default: 0},
-    subjects: {type:[
+    subjects: {type:
+      [
         {
             subject: {type:String},
             start_grade: {type:Number},
             end_grade: {type:Number}
         }
-    ], required:false},
-    availability: {type:{
-        Monday: {type: [
+      ],
+      required: true,
+      default: []
+    },
+    availability: {type:
+      {
+        Monday: {type:
+          [
             {
                 start_time: {type:String},
                 end_time: {type:String}
-            },
-            required:false
-        ]},
-        Tuesday: {type: [
+            }
+          ],
+          required: true,
+          default: []
+        },
+        Tuesday: {type:
+          [
             {
                 start_time: {type:String},
                 end_time: {type:String}
-            },
-            required:false
-        ]},
-        Wednesday: {type: [
+            }
+          ],
+          required: true,
+          default: []
+        },
+        Wednesday: {type:
+          [
             {
                 start_time: {type:String},
                 end_time: {type:String}
-            },
-            required:false
-        ]},
-        Thursday: {type: [
+            }
+          ],
+          required: true,
+          default: []
+        },
+        Thursday: {type:
+          [
             {
                 start_time: {type:String},
                 end_time: {type:String}
-            },
-            required:false
-        ]},
-        Friday: {type: [
+            }
+          ],
+          required: true,
+          default: []
+        },
+        Friday: {type:
+          [
             {
                 start_time: {type:String},
                 end_time: {type:String}
-            },
-            required:false
-        ]},
-        Saturday: {type: [
+            }
+          ],
+          required: true,
+          default: []
+        },
+        Saturday: {type:
+          [
             {
                 start_time: {type:String},
                 end_time: {type:String}
-            },
-            required: false
-        ]},
-        Sunday: {type: [
+            }
+          ],
+          required: true,
+          default: []
+        },
+        Sunday: {type:
+          [
             {
                 start_time: {type:String},
                 end_time: {type:String}
-            },
-            required: false
-        ]},
+            }
+          ],
+          required: true,
+          default: []
+        },
     }},
-    gender: {type:String},
-    online: {type:Boolean}
+    gender: {type:String, required: true},
+    online: {type:Boolean, required: true}
 
 });
 
