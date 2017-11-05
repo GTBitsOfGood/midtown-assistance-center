09445c00a530b49685e09ed75911d87b5212504b
Sruti <sruti@gatech.edu>
2017-11-05 16:51:53 -0500

    made no results found a bit better

--- src/dash/TutorSearchList.jsx
+++ src/dash/TutorSearchList.jsx
@@ -13,9 +13,9 @@ class DefaultDashTutorList extends React.Component {
     render() {
 
 
-        const renData = this.props.data.map((obj, num) => {
+        const renData = this.props.data.length > 0 ? this.props.data.map((obj, num) => {
             return <TutorSearchResult data={obj} id={num}/>;
-        });
+        }) : <h4>No Results Found</h4>;
 
         return (
             <div className="col-md-10 col-md-offset-1 tutor-list-wrapper">
@@ -25,7 +25,7 @@ class DefaultDashTutorList extends React.Component {
                 { this.props.searchType == "online" ? "" : this.props.subject + " " + this.props.time }
                 </span>
                 </h3>
-                 <div className="panel-group tutors-list" id="accordion">
+                 <div className={ "panel-group " + (this.props.data.length > 0 ? "tutors-list" : "")} id="accordion">
                   {renData}
                  </div>
             </div>
