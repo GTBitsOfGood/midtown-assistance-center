e6026611afa2f0a4c34b7699c2583b7b9117a2bd
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-11-09 21:55:56 -0500

    Refactor with redux

--- src/dash/student_view/TutorSearchList.jsx
+++ src/dash/student_view/TutorSearchList.jsx
@@ -11,20 +11,15 @@ class TutorList extends React.Component {
         const renData = this.props.studentView.filteredTutors.length > 0 ? this.props.studentView.filteredTutors.map((obj, num) => {
             return <TutorSearchResult data={obj} id={num}/>;
         }) : <h4>No Results Found</h4>;
-        const searchResults = <h3 className="text-uppercase tutors-header">Search Results for <span className='gold_text'>{"'" + this.props.subject + " " + this.props.time + "'"}</span></h3>;
-        const tutorsOnline = <h3 className="text-uppercase tutors-header">Tutors Currently Online</h3>
+        const searchResults = <h3 className="text-uppercase tutors-header">Search Results for <span className='gold_text'>{"'" + this.props.studentView.subject + " " + this.props.studentView.time + "'"}</span></h3>;
+        const tutorsOnline = <h3 className="text-uppercase tutors-header">Tutors Currently Online</h3>;
         return (
-            <div className="col-md-10 col-md-offset-1 tutor-list-wrapper">
-                <h3 className="text-uppercase tutors-header">
-                { this.props.studentView.searchType === "online" ? "Tutors Currently Online" : "Search Results for "}
-                <span className='gold_text'>
-                { this.props.studentView.searchType === "online" ? "" : this.props.studentView.searchSubject + " " + this.props.studentView.searchTime }
-                </span>
-                </h3>
-                 <div className={ "panel-group " + (this.props.studentView.filteredTutors.length > 0 ? "tutors-list" : "")} id="accordion">
-                  {renData}
-                 </div>
+          <div className="col-md-10 col-md-offset-1 tutor-list-wrapper">
+            { this.props.studentView.searchType === "online" ? tutorsOnline : searchResults}
+            <div className={ "panel-group " + (this.props.studentView.filteredTutors.length > 0 ? "tutors-list" : "")} id="accordion">
+              {renData}
             </div>
+          </div>
         );
     }
 
