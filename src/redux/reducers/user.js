4432a91ee55873baa78e09ea2de12c55c92ad46d
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-11-11 22:29:08 -0500

    Fix user reducer

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,15 +1,15 @@
 import config from 'config';
 
-export default function user(state = {}, action) {
+export default function change_user(state = {}, action) {
   let new_state = Object.assign({}, state);
 
   switch (action.type) {
     case 'UPDATE_IN_USER':
       let new_user = action.payload;
-      new_state.user = new_user;
+      new_state = new_user;
 
       // Hide password (even though our backend should already hide it)
-      new_state.user.password = config.hidden_password;
+      new_state.password = config.hidden_password;
       break;
   }
 
