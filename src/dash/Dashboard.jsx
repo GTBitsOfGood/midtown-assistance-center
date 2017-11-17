31716de75c0bd7e70f9fc968de0a57710efd9df3
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-11-16 19:53:12 -0500

    Merge stuff

--- src/dash/Dashboard.jsx
+++ src/dash/Dashboard.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomeMenuBar from '../MenuBar.jsx';
-import DefaultDash from './student_view/StudentDash.jsx';
-import DefaultAdminDash from './admin_view/AdminDash.jsx';
-import DefaultProfile from './student_view/Profile.jsx';
+import StudentDash from './student_view/StudentDash.jsx';
+import StudentProfile from './student_view/Profile.jsx';
 import { Provider } from 'react-redux';
 import store from '../redux/store.js';
 import AboutUs from '../home/AboutUs.jsx';
@@ -37,10 +36,9 @@ export class DashComp extends React.Component {
       <div className="animated fadeInDown">
         <HomeMenuBar homeordash='dash'/>
           <Router history={browserHistory}>
-          <Route path="/dash" component={DefaultDash}/>
+          <Route path="/dash" component={StudentDash}/>
           <Route path="/dash/about" component={AboutUs}/>
-          <Route path="/dash/profile" component={DefaultProfile}/>
-          <Route path="/dash/admin" component={DefaultAdminDash}/>
+          <Route path="/dash/profile" component={StudentProfile}/>
           </Router>
       </div>
     );
