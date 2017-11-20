600d4573ae18fa2b24f28bc693b9653840472ab6
Sruti <sruti@gatech.edu>
2017-11-20 17:14:53 -0500

    basic tutor dash stuff

--- src/dash/tutor_view/TutorDash.jsx
+++ src/dash/tutor_view/TutorDash.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import TutorUpcomingEvents from './TutorUpcomingEvents.jsx';
 import { connect } from 'react-redux';
 
+const tutor = {
+      first_name: 'Sruti',
+      last_name: 'Guhathakurta',
+      subjects: [{subject:'Math', start_grade:6, end_grade:12},{subject:'Science', start_grade:5, end_grade:7},{subject:'Computers', start_grade:6, end_grade:12}],
+      availability: {
+          Monday: [{start_time:'2:00', end_time:'3:00'}],
+          Tuesday: [{start_time:'6:00', end_time:'7:00'}],
+          Wednesday: [],
+          Thursday: [{start_time:'4:00', end_time:'5:00'}],
+          Friday: [],
+          Saturday: [],
+          Sunday: []
+      },
+      profile_picture: '/images/default_user_img.png',
+      bio: 'Hello, my name is Sruti.',
+      email: 'sruti@gatech.edu',
+      class_standing: 'Senior',
+      rating: 5,
+      online:false,
+      gender: 'female'
+}
+
 class TutorDash extends React.Component {
 
   render() {
@@ -10,8 +32,8 @@ class TutorDash extends React.Component {
       <div className="col-md-6">
         <h2>Profile</h2>
       </div>
-      <div className="col-md-6">
-        <TutorUpcomingEvents/>
+      <div className="col-md-6 upcoming-events-list">
+        <TutorUpcomingEvents availability={tutor.availability}/>
       </div>
     </div>
     );
--- src/dash/tutor_view/TutorUpcomingEvent.jsx
+++ src/dash/tutor_view/TutorUpcomingEvent.jsx
@@ -4,8 +4,15 @@ class TutorUpcomingEvent extends React.Component {
 
   render() {
     return (
-    <div>
-
+    <div className="tutorUpcomingEvent">
+        <div className="tutorUpcomingEventContent">
+            <h3 className="upcoming-event-desc"><strong>{this.props.today ? "Today" : this.props.dayName}</strong><span className="lighter-text"> from </span><strong>{this.props.startTime}</strong><span className="lighter-text"> to </span><strong>{this.props.endTime}</strong>
+            </h3>
+            <a href="#" className="google-link">
+            <img src={this.props.today ? "/images/google-icon-active.png" : "/images/google-icon-disabled.png"}>
+            </img>
+            </a>
+        </div>
     </div>
     );
   }
