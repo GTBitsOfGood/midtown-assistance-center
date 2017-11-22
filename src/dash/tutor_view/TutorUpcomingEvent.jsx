844edef17a0ae1995948b6a2a17ba7aa856365f9
Sruti <sruti@gatech.edu>
2017-11-22 16:31:37 -0500

    disable google anchor for other days

--- src/dash/tutor_view/TutorUpcomingEvent.jsx
+++ src/dash/tutor_view/TutorUpcomingEvent.jsx
@@ -3,15 +3,13 @@ import React from 'react';
 class TutorUpcomingEvent extends React.Component {
 
   render() {
+    const renLogo = this.props.today ? <a href="#" className="google-link"><img src="/images/google-icon-active.png"></img></a> : <img className="google-link" src="/images/google-icon-disabled.png"></img>
     return (
     <div className="tutorUpcomingEvent">
         <div className="tutorUpcomingEventContent">
             <h3 className="upcoming-event-desc"><strong>{this.props.today ? "Today" : this.props.dayName}</strong><span className="lighter-text"> from </span><strong>{this.props.startTime}</strong><span className="lighter-text"> to </span><strong>{this.props.endTime}</strong>
             </h3>
-            <a href="#" className="google-link">
-            <img src={this.props.today ? "/images/google-icon-active.png" : "/images/google-icon-disabled.png"}>
-            </img>
-            </a>
+            {renLogo}
         </div>
     </div>
     );
