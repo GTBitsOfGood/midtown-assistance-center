cc94353eddf84ee528c96f85cc5fb146d5c7b5c2
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-11-24 11:10:06 -0500

    Session dao now has a single filtering method

--- api/dao/session_dao.js
+++ api/dao/session_dao.js
@@ -6,55 +6,47 @@ module.exports = {
         Session.create(session, function (err, session_instance) {
             if (err) {
                 console.error('Error creating a new session:', err);
-                callback(err);
-            } else {
-                callback(null, session_instance);
+                return callback(err);
             }
+
+            return callback(null, session_instance);
         });
     },
 
-    getSessionsViaUsername: function(username, callback) {
-        Session.find({username: username}, function (err, docs) {
-            if (err) {
-                console.error('Error checking if any sessions exist with this username:', err);
-                callback(err);
-            } else if (docs && docs.length > 0) {
-                callback(null, docs);
-            } else {
-                callback(null, null);
-            }
-        });
+    getFilteredSessions(username, startTime, endTime, limit, callback) {
+      function filterByUsername(session) {
+        return session.username === username;
+      }
+
+      function filterAfterStartTime(session) {
+        return session.time > startTime;
+      }
+
+      function filterBeforeEndTime(session) {
+        return session.time < endTime;
+      }
+
+      Session.find({}, function (err, docs) {
+        if (err) {
+          console.error('Error checking if any sessions exist in the DataBase:', err);
+          return callback(err);
+        }
+
+        let sessions = docs;
+
+        if (username) {
+          sessions = sessions.filter(filterByUsername);
+        }
+        if (startTime) {
+          sessions = sessions.filter(filterAfterStartTime);
+        }
+        if (endTime) {
+          sessions = sessions.filter(filterBeforeEndTime);
+        }
+
+        sessions.length = Math.min(sessions.length, limit);
+
+        callback(null, sessions);
+      });
     },
-
-    // FIXME this function isn't useful, it should take in a time range not a specific time
-    getSessionsViaTime: function(time, callback) {
-        Session.find({time: time}, function (err, docs) {
-            if (err) {
-                console.error('Error checking if any sessions exist with this Time:', err);
-                callback(err);
-
-            } else if(docs && docs.length > 0){
-                callback(null, docs);
-            } else {
-                callback(null, null);
-            }
-        });
-    },
-
-    getAllSessionsInDB: function(callback) {
-        Session.find({}, function (err, docs) {
-            if (err) {
-                console.error('Error checking if any sessions exist in the DataBase:', err);
-                callback(err);
-            } else if(docs && docs.length > 0){
-                callback(null, docs);
-            } else {
-                callback(null, null);
-            }
-        });
-    },
-
-    getSessions(username, startTime, endTime, number, callback) {
-      // TODO
-    }
 };
