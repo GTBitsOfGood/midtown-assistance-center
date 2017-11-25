001730408c6f76e1fa55cad3d38267c65f83708d
rohith macherla <rohithmacherla1997@gmail.com>
2017-11-25 18:03:10 -0500

    Refactored some code with identations

--- src/dash/tutor_view/TutorDash.jsx
+++ src/dash/tutor_view/TutorDash.jsx
@@ -5,33 +5,33 @@ import TutorProfile from './Profile.jsx';
 
 class TutorDash extends React.Component {
 
-  render() {
-    return (
-    <div className="tutor-dash container col-xs-12">
-      <div className="col-md-7">
-        <TutorProfile/>
-      </div>
-      <div className="col-md-5 upcoming-events-list">
-        <TutorUpcomingEvents/>
-      </div>
-    </div>
-    );
-  }
+    render() {
+        return (
+            <div className="tutor-dash container col-xs-12">
+                <div className="col-md-7">
+                    <TutorProfile/>
+                </div>
+                <div className="col-md-5 upcoming-events-list">
+                    <TutorUpcomingEvents/>
+                </div>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  // Since we never use the redux state here
-  console.log(state);
-  return state;
+    // Since we never use the redux state here
+    console.log(state);
+    return state;
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    setUser : (user) => dispatch(updateUser(user))
-  }
+    return {
+        setUser : (user) => dispatch(updateUser(user))
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(TutorDash);
