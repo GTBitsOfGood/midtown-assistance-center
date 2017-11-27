7e342162ae07a286b6c4b97c5a8d6a3db7569d47
Sruti <sruti@gatech.edu>
2017-11-27 14:56:54 -0500

    datepicker

--- src/dash/tutor_view/TimePicker.jsx
+++ src/dash/tutor_view/TimePicker.jsx
@@ -4,9 +4,6 @@ class TimePicker extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            start: this.props.start,
-            end: this.props.end,
-            date: this.props.date,
             show: true
         };
         this.handleStartChange = this.handleStartChange.bind(this);
@@ -16,28 +13,28 @@ class TimePicker extends React.Component {
     }
 
     handleStartChange(event) {
-        this.props.handleEditStart(this.state.date, event.target.value, this.state.end);
-        this.setState({start: event.target.value });
+        this.props.handleEditStart(this.props.date, event.target.value, this.props.end);
+        //this.setState({start: event.target.value });
     }
 
     handleEndChange(event) {
-        this.props.handleEditEnd(this.state.date, this.state.start, event.target.value);
-        this.setState({end: event.target.value });
+        this.props.handleEditEnd(this.props.date, this.props.start, event.target.value);
+        //this.setState({end: event.target.value });
     }
 
     handleDateChange(event) {
-        this.props.handleEditDate(this.state.date, event.target.value, this.state.start, this.state.end);
-        this.setState({date: event.target.value});
+        this.props.handleEditDate(this.props.date, event.target.value, this.props.start, this.props.end);
+        //this.setState({date: event.target.value});
     }
 
     handleRemoveClick(event) {
-        this.setState({show:false});
-        this.props.handleRemoveSchedule(this.state.date, this.state.start, this.state.end);
+        //this.setState({show:false});
+        this.props.handleRemoveSchedule(this.props.date, this.props.start, this.props.end);
     }
 
     render() {
         const renData = <span>
-            <select defaultValue={ this.props.date } onChange={ this.handleDateChange } disabled={ !this.props.is_edit }>
+            <select defaultValue={ this.props.date } value={this.props.date} onChange={ this.handleDateChange } disabled={ !this.props.is_edit }>
                 <option value="Sunday">Sunday</option>
                 <option value="Monday">Monday</option>
                 <option value="Tuesday">Tuesday</option>
@@ -48,11 +45,11 @@ class TimePicker extends React.Component {
             </select>
             <span className="time-pick">
                 <label>Start Time: </label>
-                <input type="time" value={ this.state.start } onChange={ this.handleStartChange } disabled={ !this.props.is_edit }/>
+                <input type="time" value={ this.props.start } onChange={ this.handleStartChange } disabled={ !this.props.is_edit }/>
             </span>
             <span className="time-pick">
                 <label>End Time: </label>
-                <input type="time" value={ this.state.end } onChange={ this.handleEndChange } disabled={ !this.props.is_edit }/>
+                <input type="time" value={ this.props.end } onChange={ this.handleEndChange } disabled={ !this.props.is_edit }/>
             </span>
             <button
                 value={this.props.key}
