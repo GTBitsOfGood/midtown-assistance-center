31bdef509ba32e3077dcba7a45b30da2915f0100
Sruti <sruti@gatech.edu>
2018-02-20 21:16:33 -0500

    acl role add

--- api/calendar_api.js
+++ api/calendar_api.js
@@ -4,8 +4,52 @@ var google = require('./google_hangouts.js');
 var moment = require('moment');
 import data_access from './data_access';
 
+
+app.post('/addCalendarACL', function(req, res) {
+    let tutorId = req.body.id;
+    let email = req.body.email;
+    let calendarId = req.body.calendarId;
+    google.auth.refreshAccessToken(function(err, token) {
+      if (err) {
+            res.send({
+                success: false,
+                payload: null,
+                error: err
+            });
+            console.log('Error while trying to retrieve access token', err);
+            return;
+        }
+        google.auth.credentials = token;
+        google.calendar.acl.insert({
+          auth: google.auth,
+          calendarId: calendarId,
+          resource: {
+              'role':'owner',
+              'scope': {
+                'type': 'user',
+                'value': email
+              }
+          }
+        }, function(err, r){
+          if (err) {
+            res.send({
+                success: false,
+                payload: null,
+                error: err
+            });
+            return;
+          } else {
+            res.send({
+                success: true,
+            });
+          }
+        });
+    });
+});
+
 app.post('/createNewCalendar', function(req, res){
 	let tutorId = req.body.id;
+	let email = req.body.email;
 	google.auth.refreshAccessToken(function(err, token) {
       if (err) {
             console.log("error1");
@@ -26,8 +70,6 @@ app.post('/createNewCalendar', function(req, res){
             }
         }, function(error, response) {
             if (error) {
-                console.log("error2");
-                console.log(error);
                 res.send({
                     success: false,
                     payload: null,
@@ -37,7 +79,6 @@ app.post('/createNewCalendar', function(req, res){
             }
 
             let calId = response.id;
-            console.log("br3");
             // code to update the tutor db with the calendar for the tutor
             data_access.users.getUser(tutorId, function(err, tutor) {
                 if (err) {
@@ -51,7 +92,6 @@ app.post('/createNewCalendar', function(req, res){
 
                 // Update the tutor with the calendar id that was just created for him
                 tutor.calendarId = calId;
-
                 data_access.users.saveTutor(tutor, function(err, updatedTutor) {
                     if (err) {
                         res.send({
@@ -61,13 +101,12 @@ app.post('/createNewCalendar', function(req, res){
                         });
                         return;
                     }
-
                     res.send({
                         success: true,
+                        calId: calId,
                         payload: updatedTutor
                     });
                 });
-
             });
         });
     });
@@ -132,7 +171,8 @@ app.post('/createEvent', function(req, res){
        },
        guestsCanInviteOthers:true,
        guestsCanModify:true
-    }}, function(err, response){
+    }
+    }, function(err, response){
       if (err) {
         res.json({
           success: false,
