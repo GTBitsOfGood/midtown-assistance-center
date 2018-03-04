737e0dfe802f88eb0df8e26ff2740881e051dc5b
Sam Baek <kie827@gmail.com>
2018-03-04 01:47:01 -0500

    add Subject.js model

---
+++ models/Subject.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let schema = new Schema({
+    title: {type: String, required: true},
+});
+
+module.exports = mongoose.model('Subject', schema);
