0834a234f6bbb704f3461b1663700f053a4ffc86
Tiffany <tiffanyxhsieh@gmail.com>
2018-03-27 13:45:54 -0400

    added tracked files

--- src/admin_view/pages/Dashboard.jsx
+++ src/admin_view/pages/Dashboard.jsx
@@ -1,13 +1,21 @@
 import styles from '../../../public/css/admin.css';
 import React from 'react';
-
+import GPAImprovementStatistics from '../GPAImprovementStatistics.jsx'
+import SubjectPieStatistics from '../SubjectPieStatistics.jsx'
+import LoginStatistics from '../LoginStatistics.jsx'
+import UserList from '../UserList.jsx'
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <div>
+      <div class = "Row">
+      	<SubjectPieStatistics/>
+      	<LoginStatistics/>
+        <GPAImprovementStatistics/>
+        <UserList/>
+
       </div>
     );
   }
