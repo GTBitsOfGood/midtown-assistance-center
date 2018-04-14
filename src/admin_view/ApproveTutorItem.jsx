c0999b0c6ccfba3795e3c0b99d2d2a846087fe60
niruku7 <niruku7@gatech.edu>
2018-04-14 15:06:41 -0400

    updated approve tutor item to make it prettier/easier to read

--- src/admin_view/ApproveTutorItem.jsx
+++ src/admin_view/ApproveTutorItem.jsx
@@ -8,7 +8,7 @@ class ApproveTutorItem extends React.Component {
   }
   render() {
     return (
-      <div className="approve-tutor-item text-center container col-md-3">
+      <div className="approve-tutor-item text-center container col-md-4">
       {/** 
         <h3>{this.props.data.first_name + " " + this.props.data.last_name}</h3>
         <h4 className="lighter-text">{this.props.data.email}</h4>
@@ -17,11 +17,11 @@ class ApproveTutorItem extends React.Component {
         <button className="btn  btn-danger reject-button">REJECT</button>
       **/}
 
-        <h3>Jane Doe</h3>
-        <h4 className="lighter-text">jane.doe@gmail.com</h4>
-        <h4 className="lighter-text">Georgia Tech</h4>
+        <h2>Jane Doe</h2>
+        <h4 className="lighter-text gold_text">jane.doe@gmail.com</h4>
+        <h4 className="lighter-text gold_text">Georgia Tech</h4>
 
-        <div className={styles.admin_application_bio}>
+        <div className={styles.approve_tutor_bio}>
           <p>
             Tutor Bio goes here
           </p>
