e48c3efa443aa74f4030562d7fed7940ea3332db
Sam Baek <kie827@gmail.com>
2018-04-16 20:17:07 -0400

    revert passportConfig

--- passportConfig.js
+++ passportConfig.js
@@ -21,24 +21,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-/**
- * Configure Passport to use the Local strategy. Passport-local is
- * "A module lets you authenticate using a username and password in your Node.js applications."
- * https://github.com/jaredhanson/passport-local
- * @param Strategy
- */
-
 passport.use(
-    new LocalStrategy(
-        /**
-         * 1. We check to see if this user exists in the database.
-         * 2. We must do this to handle login in.
-         * 3. We must query the mongo-database using the user_dao.js file (we defined the interface methods here)
-         * 4. If the user doesn't exist, then we return an error or report that password was false
-         * @param username: Username
-         * @param password: Non-hashed Password
-         * @param done: A callback that has parameters (error, user object (or false if failure))
-         */
+    new LocalStrategy(function(username, password, done) {
+        // Fill in the access to MongoDB and the users within that
 
         data_access.users.getUser(username, function(err, user_instance) {
             if (err) {
@@ -46,7 +31,6 @@ passport.use(
                 return done(err);
             }
 
-            //If we were passed a null user_instance for some reason, throw an error
             if (user_instance === null) {
                 console.log('ERROR: user instance is null');
                 return done(null, false, {
@@ -58,11 +42,12 @@ passport.use(
             if (user_instance.password === password) {
                 return done(null, user_instance);
             }
+
             return done(null, false, {
                 message: 'Incorrect username or password!'
             });
-        })
-    )
+        });
+    })
 );
 
 passport.serializeUser(function(user, done) {
@@ -104,29 +89,16 @@ app.get('/logout', (req, res) => {
     res.send(true);
 });
 
-/**
- * 1. This function will login in a user of type: Tutor, Student, and Admin
- * 2. This method is called using axios in LoginForm.jsx
- * @param req: This contains the information from the frontend (namely: username, password, logInTime).
- * @param res: This contains the info being sent back to the frontend
- * @param next: ???
- */
-
 app.post('/login', function(req, res, next) {
     passport.authenticate('local', function(err, user, info) {
-        // If you find an error authenticating, print it
         if (err) {
-            console.error(err);
             return next(err);
         }
 
-        // If authentication failed, user will be set to false. (http://www.passportjs.org/docs/authenticate/)
-        //This if statement tells frontend in LoginForm that authentication failed.
         if (!user) {
             return res.send(null);
         }
 
-        //At this point, we have that authentication is successful.
         req.logIn(user, function(err) {
             if (err) {
                 return next(err);
@@ -148,10 +120,9 @@ app.post('/login', function(req, res, next) {
                 console.log(session_instance);
             });
 
-            //We tell LoginForm.jsx that a user has been properly authenticated
             return res.send(user);
         });
-    })(req, res, next); //Passport's API uses this anonymous function calling notation, don't change it.
+    })(req, res, next);
 });
 
 export default app;
