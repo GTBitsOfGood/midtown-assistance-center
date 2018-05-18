bb81ca67d3792e3cb5d90729cb041bedf1cde0cf
Sam Baek <kie827@gmail.com>
2018-05-18 01:05:54 -0400

    rid of unnecessary function

--- src/home/HomeMenuBar.jsx
+++ src/home/HomeMenuBar.jsx
@@ -2,28 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar, MenuItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import axios from 'axios';
 import styles from '../../public/css/index.css';
 
 export default class HomeMenuBar extends React.Component {
     constructor(props) {
         super(props);
-        this.logout = this.logout.bind(this);
-    }
-
-    logout() {
-        console.warn('Logging out user');
-        axios
-            .get('/logout')
-            .then(function(response) {
-                console.log(response);
-                if (response.data === true) {
-                    document.location.href = '/';
-                }
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
     }
 
     render() {
