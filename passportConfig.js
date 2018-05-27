a647de501be467ded943da0ba088eb7da8fcde53
Tiffany <tiffanyxhsieh@gmail.com>
2018-05-27 14:32:52 -0400

    added password hashing for new student and new admin
    changed login to check for an unhashed password. If it is unhashed, it will update it to be hashed

--- passportConfig.js
+++ passportConfig.js
@@ -49,9 +49,43 @@ passport.use(
                     return done(err);
                 }
                 if (!isMatch) {
-                    return done(null, false, {
-                        message: 'Incorrect username or password'
-                    });
+                    /*
+                        Check if it is an old account w/ an unhashed password.
+                        If it is, update the account's password to be hashed
+                     */
+                    if (user_instance.password === password) {
+                        //generate a salt, then run callback
+                        bcrypt.genSalt(10, function(err, salt) {
+                            if (err) {
+                                return err;
+                            }
+
+                            //hash password using salt
+                            bcrypt.hash(password, salt, function(err, hash) {
+                                if (err) {
+                                    return err;
+                                }
+
+                                console.log('hash: ' + hash);
+                                //overwrite plain text pass w/ encrypted pass
+                                user_instance.update(
+                                    { password: hash },
+                                    function(err, raw) {
+                                        if (err) return err;
+                                        console.log(
+                                            'The raw response from Mongo was ',
+                                            raw
+                                        );
+                                    }
+                                );
+                            });
+                        });
+                        return done(null, user_instance);
+                    } else {
+                        return done(null, false, {
+                            message: 'Incorrect username or password'
+                        });
+                    }
                 }
                 return done(null, user_instance);
             });
