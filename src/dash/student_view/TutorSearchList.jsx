4e35b27ff7653fb73f69ff14157e04e5e6eba575
Sammy Baek <kie827@gmail.com>
2018-09-18 23:01:38 -0400

    clean up TutorSearchList.jsx

--- src/dash/student_view/TutorSearchList.jsx
+++ src/dash/student_view/TutorSearchList.jsx
@@ -1,83 +1,74 @@
 import React from 'react';
-import TutorSearchResult from './TutorSearchResult.jsx';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import TutorSearchResult from './TutorSearchResult';
 
-class TutorList extends React.Component {
-    render() {
-        const renData =
-            this.props.studentView.filteredTutors.length > 0 ? (
-                this.props.studentView.filteredTutors.map((obj, num) => {
-                    return (
-                        <TutorSearchResult
-                            updateTutors={this.props.updateTutors}
-                            socket={this.props.socket}
-                            data={obj}
-                            id={num}
-                            studentEmail={this.props.user.email}
-                            username={this.props.user._id}
-                        />
-                    );
-                })
-            ) : (
-                <h4>No Results Found</h4>
-            );
-        const searchResults = (
-            <div>
-                <h4 className="tutors-header">
-                    Search Results for{' '}
-                    <span className="gold_text">
-                        {"'" +
-                            (this.props.studentView.searchSubject
-                                ? this.props.studentView.searchSubject + ' '
-                                : '') +
-                            this.props.studentView.searchTime +
-                            "'"}
-                    </span>
-                </h4>
-                <span>
-                    <a className="clear-search" href="/dash">
-                        <h4>Clear Search</h4>
-                    </a>
+const TutorList = (props) => {
+    const {studentView, updateTutors, socket, user} = props;
+    const renData =
+        studentView.filteredTutors.length > 0 ? (
+            studentView.filteredTutors.map((obj, num) => (
+                <TutorSearchResult
+                    updateTutors={updateTutors}
+                    socket={socket}
+                    data={obj}
+                    id={num}
+                    studentEmail={user.email}
+                    username={user._id}
+                />
+            ))
+        ) : (
+            <h4>No Results Found</h4>
+        );
+
+    const searchResults = (
+        <div>
+            <h4 className="tutors-header">
+                Search Results for{' '}
+                <span className="gold_text">
+                    {`'${(studentView.searchSubject ? `${studentView.searchSubject} ` : '')}${studentView.searchTime}'`}
                 </span>
+            </h4>
+            <span>
+                <a className="clear-search" href="/dash">
+                    <h4>Clear Search</h4>
+                </a>
+            </span>
+        </div>
+    );
+    const tutorsOnline = (
+        <h4 className="tutors-header">Tutors Currently Online</h4>
+    );
+    return (
+        <div className="col-md-10 col-md-offset-1 tutor-list-wrapper">
+            <div className="search-list-header">
+                {studentView.searchType === 'online'
+                    ? tutorsOnline
+                    : searchResults}
             </div>
-        );
-        const tutorsOnline = (
-            <h4 className="tutors-header">Tutors Currently Online</h4>
-        );
-        return (
-            <div className="col-md-10 col-md-offset-1 tutor-list-wrapper">
-                <div className="search-list-header">
-                    {this.props.studentView.searchType === 'online'
-                        ? tutorsOnline
-                        : searchResults}
-                </div>
-                <div
-                    className={
-                        'panel-group ' +
-                        (this.props.studentView.filteredTutors.length > 0
-                            ? 'tutors-list'
-                            : '')
-                    }
-                    id="accordion"
-                >
-                    {renData}
-                </div>
+            <div
+                className={`panel-group ${studentView.filteredTutors.length > 0 ? 'tutors-list' : ''}`}
+                id="accordion"
+            >
+                {renData}
             </div>
-        );
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        studentView: state.studentView,
-        user: state.user
-    };
+        </div>
+    );
 };
 
-const mapDispatchToProps = dispatch => {
-    return {};
+TutorList.propTypes = {
+    studentView: PropTypes.object.isRequired,
+    updateTutors: PropTypes.object.isRequired,
+    socket: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired
 };
 
-const TutorSearchList = connect(mapStateToProps, mapDispatchToProps)(TutorList);
+const mapStateToProps = state => ({
+    studentView: state.studentView,
+    user: state.user
+});
+
+
+const TutorSearchList = connect(mapStateToProps, null)(TutorList);
 
 export default TutorSearchList;
